674329d7766a6f2fa23ac2fdd66be8c707d6b746
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-18 22:55:42 +0530

    Create diff classes

---
+++ Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class Inventory
+    {
+        public List<TypesofRice> typesofrice;
+        public List<TypesofPulses> typesofpulses;
+        public List<TypesofWheats> typesofwheats;
+
+    }
+
+    public class TypesofRice
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+
+    }
+    public class TypesofPulses
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+
+    }
+    public class TypesofWheats
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+
+    }
+}
