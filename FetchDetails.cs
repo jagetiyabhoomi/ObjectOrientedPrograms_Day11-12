a12d0d791540ee9063b36592258a386da07a720d
jagetiyabhoomi <jagetiyabhoomi@gmail.com>
2022-10-18 22:54:27 +0530

    Fetch Details

---
+++ FetchDetails.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace InventoryManagement
+{
+    public class FetchDetails
+    {
+        public Inventory Read(string path)
+        {
+            using(StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json=file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Inventory>(json);
+
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e);
+                    return null;
+                }
+            }
+        }
+    }
+}
